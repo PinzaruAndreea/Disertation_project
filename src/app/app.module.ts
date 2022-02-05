5629f8a8d0d6863e462238db8636970e42545ca5
Andreea Pinzaru <andreea.pinzaru@nexttech.ro>
2022-02-05 13:50:17 +0200

    Created mock for header and navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './global-items/header/header.component';
+import { NavigationBarComponent } from './global-items/navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule
